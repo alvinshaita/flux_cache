4fe04379b7903978ef3204c3a9ba9ed6bfb407c0
alvinshaita <alvinshaita@gmail.com>
2026-03-22 09:01:17 +0300

    clean up

--- flux_cache/backends/memory.py
+++ flux_cache/backends/memory.py
@@ -6,14 +6,12 @@ class MemoryBackend(BaseBackend):
 		self.store = {}
 
 	def has(self, key):
-		# print("999", key in self.store)
 		return key in self.store
 
 	def get(self, key):
 		return self.store.get(key)
 
 	def set(self, key, value):
-		# print("888", key, value)
 		self.store[key] = value
 
 	def delete(self, key):
--- flux_cache/utils.py
+++ flux_cache/utils.py
@@ -5,12 +5,3 @@ def generate_cache_key(func, args, kwargs):
 	key_data = (func.__module__, func.__name__, args, kwargs)
 	raw_data = pickle.dumps(key_data)
 	return hashlib.sha256(raw_data).hexdigest()
-
-
-def one():
-	return "one"
-
-
-def two():
-	print(one())
-	return "two"
